37a28fba5444b78fa54d42269fc9d66bf7daaa59
Anton Skyrda <mr.skyrda@gmail.com>
2025-12-11 11:38:21 +0200

    create migrations scripts

---
+++ backend/src/migrations/1765445707996-TestMigration.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TestMigration1765445707996 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
